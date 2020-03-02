2895584987d7e21e88b3ab2a9c301bd45baf037f
Daniel Rudziński <danielrudzinski19@gmail.com>
2020-03-02 12:05:35 +0100

    Email helper and notifications helper.

---
+++ src/Implementation/Helpers/NotificationHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Core.Database.Models;
+
+namespace Core.Users.Implementation.Helpers
+{
+    public interface INotificationHelper
+    {
+
+    }
+
+    public class NotificationHelper : INotificationHelper
+    {
+        public string GetText(NotificationPayloadModel payload)
+        {
+            switch (payload.Type.ToLower())
+            {
+                case "document_risk_status_changed": // IntValue1 = Documents count
+                    if(payload.IntValue1 > 1) return $"<a href=''>{payload.IntValue1} documents changed their risk status. Click here to visualize these documents.</a>"; // StringValue1
+                    return $"<a href=''>The risk status of document {payload.StringValue2} changed from {payload.StringValue3} to {payload.StringValue4}.</a>"; // GuidValue1, StringValue2, StringValue3, StringValue4
+                //case "document_new":
+                //    return "<a href=''>New document <doc_id> has been created for <owner_name>.</a>";
+                //case "document_projected_deadline_changed":
+                //    return "<a href=''>Deadline changed for document <doc_id> from <old_deadline> to <new_deadline>.</a>";
+                //case "client_review_deadline_violation":
+                //    return "<a href=''>Client violated maximum review time (<num_days> days) for document <doc_id>.</a>";
+                //case "subcontractor_is_late":
+                //    return "<a href=''>Subcontractor <subcontractor_name> has not submitted document <doc_id> yet. Document risk status changed to <new_status>.</a>";
+            }
+            return "";
+        }
+
+        //public string GetText(NotificationPayloadModel[] payload)
+        //{
+        //    foreach (var notification in payload.GroupBy(x => x.Type))
+        //        switch (notification.Key.ToLower())
+        //        {
+        //            case "document_risk_status_changed":
+        //                return $"<a href=''>{notification.Count()} documents changed their risk status. Click here to visualize these documents.</a>";
+        //            //case "document_new":
+        //            //    return "<a href=''><num_docs> documents were created. Click here to visualize these documents.</a>";
+        //            //case "document_projected_deadline_changed":
+        //            //    return "<a href=''>Deadline changed for <num_docs> documents. Click here to visualize affected documents.</a>";
+        //            //case "client_review_deadline_violation":
+        //            //    return "<a href=''>Client violated maximum review time for <num_docs> documents. Click here to visualize these documents.</a>";
+        //            //case "subcontractor_is_late":
+        //            //    return "<a href=''><num_docs> documents changed risk status because subcontractor <subcontractor_name> has not submitted them yet. Click here to visualize these documents.</a>";
+        //        }
+        //    return "";
+        //}
+    }
+}
---
+++ src/Implementation/Helpers/SendMailHelper.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MimeKit;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace Core.Users.Implementation.Helpers
+{
+    public interface IEmailHelper
+    {
+        bool Send(string toEmail, string toName, string subject, string body);
+    }
+
+    public class EmailHelper : IEmailHelper
+    {
+        public static MailboxConfig Config { get; set; }
+        public bool Send(string toEmail, string toName, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(Config.DisplayName, Config.Username));
+            message.To.Add(new MailboxAddress(toName, toEmail));
+            message.Subject = subject;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect(Config.Url, 587, false);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate(Config.Username, Config.Password);
+
+                client.Send(message);
+                client.Disconnect(true);
+                return true;
+            }
+        }
+
+        public class MailboxConfig
+        {
+            public string DisplayName { get; set; }
+            public string Url { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
